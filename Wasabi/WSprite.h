8c919308709a9f44e317e1cb2e405618b06f3f48
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2016-07-09 03:44:53 +0300

    Finished sprite implementation

--- Wasabi/WSprite.h
+++ Wasabi/WSprite.h
@@ -6,16 +6,45 @@ class WSprite : public WBase {
 	virtual std::string GetTypeName() const;
 
 	bool			m_hidden;
+	class WImage*	m_img;
+	WVector2		m_pos;
+	WVector2		m_size;
+	WVector2		m_rotationCenter;
+	float			m_angle;
+	unsigned int	m_priority;
+	float			m_alpha;
 
 public:
 	WSprite(Wasabi* const app, unsigned int ID = 0);
 	~WSprite();
 
-	void Render();
+	void			SetImage(WImage* img);
+	void			SetPosition(float x, float y);
+	void			SetPosition(WVector2 pos);
+	void			Move(float units);
+	void			SetSize(float sizeX, float sizeY);
+	void			SetSize(WVector2 size);
+	void			SetAngle(float fAngle);
+	void			Rotate(float fAngle);
+	void			SetRotationCenter(float x, float y);
+	void			SetRotationCenter(WVector2 center);
+	void			SetPriority(unsigned int priority);
+	void			SetAlpha(float fAlpha);
+
+	void			Render();
 
 	void			Show();
 	void			Hide();
 	bool			Hidden() const;
+
+	float			GetAngle() const;
+	float			GetPositionX() const;
+	float			GetPositionY() const;
+	WVector2		GetPosition() const;
+	float			GetSizeX() const;
+	float			GetSizeY() const;
+	WVector2		GetSize() const;
+	unsigned int	GetPriority() const;
 
 	virtual bool	Valid() const;
 };
