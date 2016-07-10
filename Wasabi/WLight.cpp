8217b12285c931f848e06319f996c5fd699a78c0
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2016-07-11 00:06:39 +0300

    More lights base implementation

--- Wasabi/WLight.cpp
+++ Wasabi/WLight.cpp
@@ -1,6 +1,6 @@
 #include "WLight.h"
 
-WLightManager::WLightManager(class Wasabi* const app) : WManager<WLight>(app) {
+WLightManager::WLightManager(Wasabi* const app) : WManager<WLight>(app) {
 
 }
 
@@ -16,7 +16,8 @@ WError WLightManager::Load() {
 
 }
 
-WLight::WLight(class Wasabi* const app, unsigned int ID) : WBase(app, ID) {
+WLight::WLight(Wasabi* const app, unsigned int ID) : WBase(app, ID) {
+	m_hidden = false;
 
 	m_app->LightManager->AddEntity(this);
 }
@@ -30,5 +31,57 @@ std::string WLight::GetTypeName() const {
 }
 
 bool WLight::Valid() const {
-	return true; // TODO: fill
+	return true;
+}
+
+void WLight::Show() {
+	m_hidden = false;
+}
+
+void WLight::Hide() {
+	m_hidden = true;
+}
+
+bool WLight::Hidden() const {
+	return m_hidden;
+}
+
+WMatrix WLight::GetWorldMatrix() {
+	UpdateLocals(); //return matrix without offsets
+	return m_WorldM;
+}
+
+bool WLight::UpdateLocals() {
+	if (m_bAltered) {
+		m_bAltered = false;
+		WVector3 _up = GetUVector();
+		WVector3 _look = GetLVector();
+		WVector3 _right = GetRVector();
+		WVector3 _pos = GetPosition();
+
+		//
+		// the world matrix is the view matrix's inverse
+		// so we build a normal view matrix and invert it
+		//
+
+		// build world matrix
+		float x = -WVec3Dot(_right, _pos);
+		float y = -WVec3Dot(_up, _pos);
+		float z = -WVec3Dot(_look, _pos);
+		(m_WorldM)(0, 0) = _right.x; (m_WorldM)(0, 1) = _up.x; (m_WorldM)(0, 2) = _look.x; (m_WorldM)(0, 3) = 0.0f;
+		(m_WorldM)(1, 0) = _right.y; (m_WorldM)(1, 1) = _up.y; (m_WorldM)(1, 2) = _look.y; (m_WorldM)(1, 3) = 0.0f;
+		(m_WorldM)(2, 0) = _right.z; (m_WorldM)(2, 1) = _up.z; (m_WorldM)(2, 2) = _look.z; (m_WorldM)(2, 3) = 0.0f;
+		(m_WorldM)(3, 0) = x;        (m_WorldM)(3, 1) = y;     (m_WorldM)(3, 2) = z;       (m_WorldM)(3, 3) = 1.0f;
+		m_WorldM = WMatrixInverse(m_WorldM);
+
+		return true;
+	}
+
+	return false;
+}
+
+void WLight::OnStateChange(STATE_CHANGE_TYPE type) {
+	WOrientation::OnStateChange(type);
+
+	m_bAltered = true;
 }
--- Wasabi/WLight.h
+++ Wasabi/WLight.h
@@ -2,14 +2,27 @@
 
 #include "Wasabi.h"
 
-class WLight : public WBase {
+class WLight : public WBase, public WOrientation {
 	virtual std::string GetTypeName() const;
 
 public:
 	WLight(class Wasabi* const app, unsigned int ID);
 	~WLight();
 
-	virtual bool Valid() const;
+	void			Show();
+	void			Hide();
+	bool			Hidden() const;
+
+	WMatrix			GetWorldMatrix();
+	bool			UpdateLocals();
+
+	virtual bool	Valid() const;
+	virtual void	OnStateChange(STATE_CHANGE_TYPE type);
+
+private:
+	bool			m_hidden;
+	bool			m_bAltered;
+	WMatrix			m_WorldM;
 };
 
 class WLightManager : public WManager<WLight> {
