82ac8996f3ce4ee760b5ca6c054945de9cfb1d06
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2019-01-11 13:47:55 +0300

    Fixed primitive topology (now actually uses user's choice)

--- Wasabi/Materials/WEffect.cpp
+++ Wasabi/Materials/WEffect.cpp
@@ -352,7 +352,7 @@ WError WEffect::BuildPipeline(WRenderTarget* rt) {
 	//IA state
 	VkPipelineInputAssemblyStateCreateInfo inputAssemblyState = {};
 	inputAssemblyState.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-	inputAssemblyState.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+	inputAssemblyState.topology = m_topology;
 
 	// Color blend state
 	VkPipelineColorBlendStateCreateInfo colorBlendState = {};
