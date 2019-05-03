043a381fd4f120750a818a65e3888e257ccb6898
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2019-05-03 20:41:36 +0300

    Combined all lighting shader code in one file

--- Wasabi/Renderers/DeferredRenderer/Shaders/dirlight.frag.glsl
+++ Wasabi/Renderers/DeferredRenderer/Shaders/dirlight.frag.glsl
@@ -1,6 +1,9 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_ARB_shading_language_420pack : enable
+#extension GL_GOOGLE_include_directive : enable
+
+#include "../../Common/Shaders/lighting_utils.glsl"
 
 layout(location = 0) in vec2 inUV;
 layout(location = 0) out vec4 outFragColor;
@@ -23,28 +26,6 @@ layout(set = 1, binding = 3) uniform UBO {
 	mat4 projInv;
 } uboPerFrame;
 
-vec4 DirectionalLight(vec3 pos, vec3 norm)
-{
-	vec3 lDir = -normalize(uboPerLight.lightDir);
-	// N dot L lighting term
-	float nl = dot(norm, lDir);
-	if (nl < 0)
-		return vec4(0, 0, 0, 0);
-
-	vec3 camDir = normalize(-pos); // since pos is in view space
-
-	// Calculate specular term
-	vec3 h = normalize(lDir + camDir);
-	float spec = pow(clamp(dot(norm, h), 0, 1), uboPerLight.lightSpec);
-
-	// Fill the light buffer:
-	// R: Color.r * N.L
-	// G: Color.g * N.L
-	// B: Color.b * N.L
-	// A: Specular Term
-	return vec4 (uboPerLight.lightColor * nl, spec);
-}
-
 void main() {
 	float z = texture(depthTexture, inUV).r;
 	float x = inUV.x * 2.0f - 1.0f;
@@ -56,6 +37,14 @@ void main() {
 
 	//clip(normalT.x + normalT.y + normalT.z - 0.01); // reject pixel
 	vec3 pixelNormal = normalize((normalT.xyz * 2.0f) - 1.0f);
-	vec4 light = DirectionalLight(pixelPosition, pixelNormal);
+	vec3 camDir = normalize(-pixelPosition); // since pixelPosition is in view space
+	vec4 light = DirectionalLight(
+		pixelPosition,
+		pixelNormal,
+		camDir,
+		uboPerLight.lightDir,
+		uboPerLight.lightColor,
+		uboPerLight.lightSpec
+	);
 	outFragColor = light * uboPerLight.intensity; //scale by intensity
 }
--- Wasabi/Renderers/DeferredRenderer/Shaders/spotlight.frag.glsl
+++ Wasabi/Renderers/DeferredRenderer/Shaders/spotlight.frag.glsl
@@ -1,6 +1,9 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_ARB_shading_language_420pack : enable
+#extension GL_GOOGLE_include_directive : enable
+
+#include "../../Common/Shaders/lighting_utils.glsl"
 
 layout(location = 0) in vec4 inPos;
 layout(location = 0) out vec4 outFragColor;
@@ -24,39 +27,6 @@ layout(set = 1, binding = 3) uniform UBOPerFrame {
 	mat4 projInv;
 } uboPerFrame;
 
-vec4 PointLight(vec3 pos, vec3 norm) {
-	vec3 lightVec = uboPerLight.position - pos;
-	float d = length(lightVec); // The distance from surface to light.
-
-	vec3 lDir = normalize(lightVec);
-
-	// N dot L lighting term
-	float nl = max(0, dot(norm, lDir));
-
-	vec3 camDir = normalize(-pos); // since pos is in view space
-
-	// Calculate specular term
-	vec3 h = normalize(lDir + camDir);
-	float spec = pow(clamp(dot(norm, h), 0, 1), uboPerLight.lightSpec);
-
-	float xVal = max(0, (1.0f - d / uboPerLight.range));
-	return vec4(uboPerLight.lightColor * nl, spec) * xVal;
-}
-
-vec4 Spotlight(vec3 pos, vec3 norm) {
-	vec4 color = PointLight(pos, norm);
-	
-	// The vector from the surface to the light.
-	vec3 lightVec = normalize(uboPerLight.position - pos);
-
-	float cosAngle = dot(-lightVec, uboPerLight.lightDir);
-	float angleFactor = max((cosAngle - uboPerLight.minCosAngle) / (1.0f - uboPerLight.minCosAngle), 0);
-	color *= angleFactor;
-	
-	// Scale color by spotlight factor.
-	return color;
-}
-
 void main() {
 	vec2 uv = (inPos.xy/inPos.w + 1) / 2;
 	float z = texture(depthTexture, uv).r;
@@ -69,6 +39,17 @@ void main() {
 
 	//clip(normalT.x + normalT.y + normalT.z - 0.01); // reject pixel
 	vec3 pixelNormal = normalize((normalT.xyz * 2.0f) - 1.0f);
-	vec4 light = Spotlight(pixelPosition, pixelNormal);
+	vec3 camDir = normalize(-pixelPosition); // since pixelPosition is in view space
+	vec4 light = SpotLight(
+		pixelPosition,
+		pixelNormal,
+		camDir,
+		uboPerLight.position,
+		uboPerLight.lightDir,
+		uboPerLight.lightColor,
+		uboPerLight.lightSpec,
+		uboPerLight.range,
+		uboPerLight.minCosAngle
+	);
 	outFragColor = light* uboPerLight.intensity; //scale by intensity
 }
