78bc39ca5ce56ac87d89dfb13cefa31cd0507311
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2019-05-10 14:10:36 +0300

    Added CreateTerrain() to TerrainManager

--- Wasabi Test/Terrain/Terrain.cpp
+++ Wasabi Test/Terrain/Terrain.cpp
@@ -7,10 +7,9 @@ TerrainDemo::TerrainDemo(Wasabi* const app) : WTestState(app) {
 }
 
 void TerrainDemo::Load() {
-	terrain = new WTerrain(m_app);
-	terrain->Create();
+	terrain = m_app->TerrainManager->CreateTerrain();
 
-	player = new WObject(m_app);
+	player = m_app->ObjectManager->CreateObject();
 	WGeometry* g = new WGeometry(m_app);
 	g->CreateCube(1.0f);
 	player->SetGeometry(g);
@@ -27,13 +26,15 @@ void TerrainDemo::Update(float fDeltaTime) {
 	if (m_app->WindowAndInputComponent->KeyDown('D'))
 		playerPos.x += 10.0f * fDeltaTime;
 
-	playerPos.y = terrain->GetHeight(WVector2(playerPos.x, playerPos.z));
-	((WasabiTester*)m_app)->SetCameraPosition(playerPos);
-	player->SetPosition(playerPos);
-	terrain->SetViewpoint(playerPos);
+	if (terrain && player) {
+		playerPos.y = terrain->GetHeight(WVector2(playerPos.x, playerPos.z));
+		((WasabiTester*)m_app)->SetCameraPosition(playerPos);
+		player->SetPosition(playerPos);
+		terrain->SetViewpoint(playerPos);
+	}
 }
 
 void TerrainDemo::Cleanup() {
-	player->RemoveReference();
-	terrain->RemoveReference();
+	W_SAFE_REMOVEREF(player);
+	W_SAFE_REMOVEREF(terrain);
 }
