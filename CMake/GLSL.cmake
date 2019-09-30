b177b1431ba34374b226cde4fa9b692a91834db0
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2019-09-30 17:32:28 +0300

    Better python3 detection in cmake

--- CMake/GLSL.cmake
+++ CMake/GLSL.cmake
@@ -1,8 +1,14 @@
 
 # adds a custom target that compiles the GLSL code using the compile-glsl-code.py python script
 function(build_glsl TARGET_NAME VULKAN_SDK_PATH)
+    # Make sure python is installed
+    find_package(Python3 COMPONENTS Interpreter)
+    if (NOT Python3_FOUND)
+        message(FATAL_ERROR "Could not find python3!")
+    endif()
+
     add_custom_target(${TARGET_NAME}
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/src/
-        COMMAND python3 compile-glsl-code.py ${VULKAN_SDK_PATH}
+        COMMAND ${Python3_EXECUTABLE} compile-glsl-code.py ${VULKAN_SDK_PATH}
     )
 endfunction()
