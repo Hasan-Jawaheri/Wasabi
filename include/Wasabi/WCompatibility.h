cb7d864269ce45323c68133d574abf7d18e7d508
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2019-10-04 10:10:16 +0300

    Fixed std::min/max on windows

--- include/Wasabi/WCompatibility.h
+++ include/Wasabi/WCompatibility.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #ifndef _WIN32
+
 #define fopen_s(a,b,c) *a = fopen(b, c)
 #define strcpy_s(a,b,c) strcpy(a,c)
 #define sprintf_s(a,b,c,...) sprintf(a,c, __VA_ARGS__)
@@ -14,4 +15,11 @@
 #ifndef UNREFERENCED_PARAMETER
 #define UNREFERENCED_PARAMETER(p) ((void)p)
 #endif
+
+#else // !defined(_WIN32)
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #endif
