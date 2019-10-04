a9935606e4fb528c5e8e813cb117972d57003070
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2019-10-04 10:04:51 +0300

    Fixed warning on Windows

--- include/Wasabi/WCompatibility.h
+++ include/Wasabi/WCompatibility.h
@@ -10,8 +10,8 @@
 #define strcpy_s(a,b,c) strcpy(a,c)
 #define sprintf_s(a,b,c,...) sprintf(a,c, __VA_ARGS__)
 #define ZeroMemory(x,y) memset(x, 0, y)
-#endif
 
 #ifndef UNREFERENCED_PARAMETER
 #define UNREFERENCED_PARAMETER(p) ((void)p)
+#endif
 #endif
