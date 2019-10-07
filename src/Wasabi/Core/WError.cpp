61bb6e08c13afe308b852afd2152cba556416e25
hasan-jawaheri <hasan.aljawaheri@gmail.com>
2019-10-07 22:13:44 +0300

    Fixed errors switch statement

--- src/Wasabi/Core/WError.cpp
+++ src/Wasabi/Core/WError.cpp
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <execinfo.h>
 #include <cxxabi.h>
-#elif defined(__OSX__)
+#elif defined(__APPLE__)
 #endif
 
 static std::string getBacktraceString();
@@ -25,30 +25,55 @@ std::string WError::AsString(bool getBacktrace) const {
 	const char* error;
 	switch (m_error) {
 	case W_ERRORUNK: error = "Unkown error occurred";
+		break;
 	case W_SUCCEEDED: error = "Succeeded";
+		break;
 	case W_WINDOWNOTCREATED: error = "Failed to create the main window";
+		break;
 	case W_CLASSREGISTERFAILED: error = "Failed to register the windows window class";
+		break;
 	case W_UNABLETOCREATEDEVICE: error = "Failed to create a Vulkan device";
+		break;
 	case W_UNABLETOCREATEIMAGE: error = "Failed to create the Vulkan image view";
+		break;
 	case W_UNABLETOCREATEBUFFER: error = "Failed to crate a buffer";
+		break;
 	case W_NOTVALID: error = "The object being used is not valid";
+		break;
 	case W_FILENOTFOUND: error = "The specified file cannot be found on the system";
+		break;
 	case W_UNABLETOCREATESWAPCHAIN: error = "Failed to create the swap chain";
+		break;
 	case W_INVALIDPARAM: error = "At least one invalid parameter was passed to the function";
+		break;
 	case W_UNABLETOMAPBUFFER: error = "Failed to map the memory buffer";
+		break;
 	case W_PHYSICSNOTINITIALIZED: error = "The physics component hasn't been initialize";
+		break;
 	case W_INVALIDFILEFORMAT: error = "The file format is not valid";
+		break;
 	case W_FAILEDTOCREATEINSTANCE: error = "Failed to create the Vulkan instance";
+		break;
 	case W_FAILEDTOLISTDEVICES: error = "Failed to enumerate Vulkan devices";
+		break;
 	case W_HARDWARENOTSUPPORTED: error = "The hardware doesn't have the minimum requirements to run the engine";
+		break;
 	case W_OUTOFMEMORY: error = "System is out of memory";
+		break;
 	case W_FAILEDTOCREATEPIPELINE: error = "Failed to create the Vulkan graphics or compute pipeline";
+		break;
 	case W_FAILEDTOCREATEDESCRIPTORSETLAYOUT: error = "Failed to create the Vulkan descriptor set layout";
+		break;
 	case W_FAILEDTOCREATEPIPELINELAYOUT: error = "Failed to create the Vulkan pipeline layout";
+		break;
 	case W_NORENDERTARGET: error = "The render target is invalid for this use";
+		break;
 	case W_INVALIDREPEATEDBINDINGINDEX: error = "The effect contains two shaders that use the same binding index for two different resources";
+		break;
 	case W_ALREADYLOADED: error = "Asset with the given name is already loaded, use GetAsset instead";
+		break;
 	case W_NAMECONFLICT: error = "Another asset with the same name is already saved";
+		break;
 	default: error = "Invalid error code";
 	}
 
@@ -86,7 +111,7 @@ static std::string getBacktraceString() {
 static std::string getBacktraceString() {
 	return "";
 }
-#elif defined(__OSX__)
+#elif defined(__APPLE__)
 static std::string getBacktraceString() {
 	return "";
 }
