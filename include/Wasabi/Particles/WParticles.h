d8f5722f8ea421e80e559913ac50b207b354f9d1
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2019-10-09 20:35:09 +0300

    Remove geometry shader using from particles (#26)
    * #11 Finished implementation using triangle strips.
    
    * Renamed particles 'type' to 'effect type'
    
    * Halfway implemented using instancing
    
    * #11 Finished implementation with instancing

--- include/Wasabi/Particles/WParticles.h
+++ include/Wasabi/Particles/WParticles.h
@@ -1,8 +1,8 @@
 /** @file WParticles.h
  *  @brief Particle systems in Wasabi
  *
- *  Particle systems in Wasabi are point-based geometries that render into
- *  quads (in a geometry shader) 
+ *  Particle systems in Wasabi are billboard-like geometries that are updated
+ *  per frame to face the screen
  *
  *  @author Hasan Al-Jawaheri (hbj)
  *  @bug No known bugs.
@@ -15,15 +15,35 @@
 
 /** A vertex of a particle */
 struct WParticlesVertex {
-	/** local-position of the single particle */
+	/** local-space position of the single particle vertex */
 	WVector3 pos;
-	/** particle size */
-	WVector3 size;
-	/** Tiling in the particle texture: 4-component vector where x,y are uv at top left,
-	    z,w are uv at bottom right */
-	WVector4 UVs;
-	/** particle color (multiplied by texture) */
-	WColor color;
+};
+
+/** An instance of a particle */
+struct WParticlesInstance {
+	/** Encoded WVP transformation matrix: (m11, m12, m13, m41) */
+	WVector4 mat1;
+	/** Encoded WVP transformation matrix: (m21, m22, m23, m42) */
+	WVector4 mat2;
+	/** Encoded WVP transformation matrix: (m31, m32, m33, m43) */
+	WVector4 mat3;
+	/**
+	 * Encoded particle color (multiplied by texture color) and UVs:
+	 * - The integer component of each float is rgba [0-255]
+	 * - The floating point component of each float is (uv top-left, uv bottom-right) [0.01-0.99]
+	 */
+	WVector4 colorAndUVs;
+	/** Size multiplier applied before any transformation (in local space) in the VS */
+	float sizeLocalSpace;
+	/** Size added to the x and y components of the particles in view space */
+	float sizeViewSpace;
+	/** Padding to be aligned to pixel size */
+	float pad1;
+	/** Padding to be aligned to pixel size */
+	float pad2;
+
+	/** Utility to encode the particle instance parameters into its fields */
+	inline void SetParameters(const WMatrix& WVP, WColor color, WVector2 uvTopLeft, WVector2 uvBottomRight, float localSize, float viewSize);
 };
 
 /** Type of default particle effects supported by WParticlesManager */
@@ -51,48 +71,79 @@ class WParticlesBehavior {
 	uint32_t m_particleSize;
 	/** Number of particles currently active */
 	uint32_t m_numParticles;
-	/** Buffer to store particles data */
-	void* m_buffer;
+	/** Buffer to store particles data (per-particle, not vertex) */
+	void* m_particlesData;
 
 protected:
+	/**
+	 * Emits a particle if not at the limit. Emittion means placing the
+	 * particle data in m_buffer and incrementing m_numParticles.
+	 * @param particle  Particle to emit
+	 */
 	void Emit(void* particle);
 
 public:
 	/**
-	 * @param max_particles  Maximum number of particles that the target system
-	 *                       can render
-	 * @param particle_size  Size of each particle structure
+	 * @param maxParticles  Maximum number of particles that the target system
+	 *                      can render
+	 * @param particleSize  Size of each particle structure
 	 */
-	WParticlesBehavior(uint32_t max_particles, uint32_t particle_size);
+	WParticlesBehavior(uint32_t maxParticles, uint32_t particleSize);
 
 	~WParticlesBehavior();
 
 	/**
 	 * Called by the WParticles implementation to update the particles and fill
-	 * in the particles vertex buffer. The size of the vertex buffer is
-	 * m_maxParticles * sizeof(WParticlesVertex)
-	 * @param curTime  The time elapsed since the beginning of the program
-	 * @param vb       Pointer to the vertex buffer to be filled
-	 * @return         Number of particles copied
+	 * in the particles buffer with instance data. The size of the buffer is
+	 * m_maxParticles * sizeof(WParticlesInstance)
+	 * @param curTime      The time elapsed since the beginning of the program
+	 * @param buffer       Pointer to the buffer to be filled
+	 * @param maxParticles Maximum number of particles in the vb
+	 * @param worldMatrix  The world matrix of the particles system
+	 * @param camera       The camera used to render the frame
+	 * @return             Number of particles copied
 	 */
-	uint32_t UpdateAndCopyToVB(float cur_time, void* vb, uint32_t max_particles);
+	uint32_t UpdateAndCopyToBuffer(float curTime, void* buffer, uint32_t maxParticles, const WMatrix& worldMatrix, class WCamera* camera);
 
 	/**
 	 * Must be implemented by a derived class to define the per-frame behavior
 	 * of the particle system. Typically this should control emission by
 	 * calling Emit() when a particle needs to be emitted.
-	 * @param cur_time The time elapsed since the beginning of the program
+	 * @param curTime     The time elapsed since the beginning of the program
+	 * @param worldMatrix The world matrix of the camera rendering the particles
+	 * @param camera      The camera used to render the frame
 	 */
-	virtual void UpdateSystem(float cur_time) = 0;
+	virtual void UpdateSystem(float curTime, const WMatrix& worldMatrix, class WCamera* camera) = 0;
 
 	/**
 	 * Must be implemented by a derived class to define the per-frame behavior
 	 * of a single particle.
-	 * @param cur_time  The time elapsed since the beginning of the program
-	 * @param particle  The particle to update
+	 * @param curTime        The time elapsed since the beginning of the program
+	 * @param particleData   A pointer to the beginning of the vertices memory to
+	 *                       fill. The memory will have sizeof(WParticlesInstance)
+	 *                       bytes to be filled with the 4 vertices of the particle
+	 *                       billboard. The output vertex must be in *VIEW SPACE*
+	 * @param outputInstance A pointer to the buffer to be filled with the instance
+	 *                       data for the particle particleData. The transformation
+	 *                       matrix is used as-is to transform from local space to
+	 *                       projection space
+	 * @param worldMatrix    The world matrix of the particles system
+	 * @param camera         The camera used to render the frame
 	 * @return  True if the particle should die, false otherwise
 	 */
-	virtual inline bool UpdateParticle(float cur_time, void* particle) = 0;
+	virtual inline bool UpdateParticle(float curTime, void* particleData, WParticlesInstance* outputInstance, const WMatrix& worldMatrix, class WCamera* camera) = 0;
+
+	/**
+	 * Must be implemented to return the minimum point in the bounding box of the
+	 * particles in local space of the instances.
+	 */
+	virtual inline WVector3& GetMinPoint() = 0;
+
+	/**
+	 * Must be implemented to return the maximum point in the bounding box of the
+	 * particles in local space of the instances.
+	 */
+	virtual inline WVector3& GetMaxPoint() = 0;
 };
 
 /**
@@ -101,10 +152,12 @@ public:
  */
 class WDefaultParticleBehavior : public WParticlesBehavior {
 	struct Particle {
-		/** Required WParticleVertex at the beginning of the data structure */
-		WParticlesVertex vtx;
 		/** Spawn time of this particle */
 		float spawnTime;
+		/** UV coordinates of the top-left vertex */
+		WVector2 UVTopLeft;
+		/** UV coordinates of the bottom-right vertex */
+		WVector2 UVBottomRight;
 		/** Initial position of this particle */
 		WVector3 initialPos;
 		/** Velocity of this particle */
@@ -113,6 +166,7 @@ class WDefaultParticleBehavior : public WParticlesBehavior {
 
 	/** Time of last emission */
 	float m_lastEmit;
+	WVector3 m_minPoint, m_maxPoint;
 
 public:
 
@@ -155,9 +209,11 @@ public:
 		(default is {<(1,1,1,0), 0.2>, <(1,1,1,1), 0.8>, <(1,1,1,0), 0.0>} */
 	std::vector<std::pair<WColor, float>> m_colorGradient;
 
-	WDefaultParticleBehavior(uint32_t max_particles);
-	virtual void UpdateSystem(float cur_time);
-	virtual inline bool UpdateParticle(float cur_time, void* particle);
+	WDefaultParticleBehavior(uint32_t maxParticles);
+	virtual void UpdateSystem(float curTime, const WMatrix& worldMatrix, class WCamera* camera);
+	virtual inline bool UpdateParticle(float curTime, void* particleData, WParticlesInstance* outputInstance, const WMatrix& worldMatrix, class WCamera* camera);
+	virtual inline WVector3& GetMinPoint();
+	virtual inline WVector3& GetMaxPoint();
 };
 
 /**
@@ -229,9 +285,9 @@ public:
 	uint32_t GetPriority() const;
 
 	/**
-	 * @return the type of this particles system
+	 * @return the effect type of this particles system
 	 */
-	W_DEFAULT_PARTICLE_EFFECT_TYPE GetType() const;
+	W_DEFAULT_PARTICLE_EFFECT_TYPE GetEffectType() const;
 
 	/**
 	 * Shows the particle system so that it can be rendered.
@@ -290,7 +346,7 @@ public:
 
 private:
 	/** Type of tyhis particle system */
-	W_DEFAULT_PARTICLE_EFFECT_TYPE m_type;
+	W_DEFAULT_PARTICLE_EFFECT_TYPE m_effectType;
 	/** true if the world matrix needs to be updated, false otherwise */
 	bool m_bAltered;
 	/** true if the particle system is hidden, false otherwise */
@@ -306,8 +362,8 @@ private:
 
 	/** Behavior of the particle system */
 	WParticlesBehavior* m_behavior;
-	/** Geometry of points used to render the particles */
-	class WGeometry* m_geometry;
+	/** Texture containing instancing data */
+	class WImage* m_instancesTexture;
 
 	/**
 	 * Destroys all resources held by this particles system
@@ -360,8 +416,8 @@ public:
 private:
 	/** Default vertex shader used by the default effect */
 	class WShader* m_vertexShader;
-	/** Default geometry shader used by the default effect */
-	class WShader* m_geometryShader;
 	/** Default fragment shader used by the default effect */
 	class WShader* m_fragmentShader;
+	/** Geometry of a plain used to render a single particle */
+	class WGeometry* m_plainGeometry;
 };
