c4c544d35dbc8e94ac1eee09cefd4e2d69635624
hasan-jawaheri <hasan.aljawaheri@gmail.com>
2019-10-19 14:55:58 +0300

    Changed GLSL compiler location for windows/unix

--- CMake/GLSL.cmake
+++ CMake/GLSL.cmake
@@ -12,10 +12,14 @@ function(build_glsl)
         message(FATAL_ERROR "Could not find python3!")
     endif()
 
-    set(GLSL_COMPILED_SOURCES "")
-    set(glslangValidator ${FUNCTION_ARGS_VULKAN}/Bin/glslangValidator.exe)
+    if(WIN32)
+        set(glslangValidator ${FUNCTION_ARGS_VULKAN}/Bin/glslangValidator.exe)
+    else()
+        set(glslangValidator ${FUNCTION_ARGS_VULKAN}/bin/glslangValidator)
+    endif()
     set(spvConverter ${CMAKE_SOURCE_DIR}/src/spv-converter.py)
     set(entryPoint "main")
+    set(GLSL_COMPILED_SOURCES "")
     foreach(file IN LISTS FUNCTION_ARGS_SOURCES)
         if(file MATCHES ".*.vert.glsl$" OR file MATCHES ".*.frag.glsl$" OR file MATCHES ".*.geom.glsl$" OR file MATCHES ".*.tesc.glsl$" OR file MATCHES ".*.tese.glsl$" OR file MATCHES ".*.comp.glsl$")
             set(tmpFile ${file}.tmp.spv)
@@ -26,7 +30,6 @@ function(build_glsl)
                 COMMAND ${glslangValidator} "-V" "--entry-point" "${entryPoint}" "${file}" "-o" "${tmpFile}"
                 COMMAND ${Python3_EXECUTABLE} ${spvConverter} "${tmpFile}" "${outputFile}"
                 COMMAND ${CMAKE_COMMAND} -E remove -f ${tmpFile}
-                COMMENT "Compiling ${file}"
                 SOURCES ${file}
                 VERBATIM
             )
