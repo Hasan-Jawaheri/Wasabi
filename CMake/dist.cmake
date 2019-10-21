0e725c1f79a41a04cd84fb7342704263243b3fc6
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2019-10-21 11:16:18 +0300

    Fixed dist/ not remade after code changes

--- CMake/dist.cmake
+++ CMake/dist.cmake
@@ -6,8 +6,9 @@ function(build_dist TARGET_NAME WASABI_TARGET_NAME)
         add_custom_command(
             OUTPUT "dist/lib/Debug/${OUTPUT_LIBNAME}" "dist/lib/Release/${OUTPUT_LIBNAME}"
             COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_SOURCE_DIR}/include/Wasabi/" "${CMAKE_BINARY_DIR}/dist/include/Wasabi/"
-            COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:wasabi> "$<$<CONFIG:Debug>:${CMAKE_BINARY_DIR}/dist/lib/Debug/$<TARGET_FILE_NAME:${WASABI_TARGET_NAME}>>$<$<CONFIG:Release>:${CMAKE_BINARY_DIR}/dist/lib/Release/$<TARGET_FILE_NAME:${WASABI_TARGET_NAME}>>"
+            COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${WASABI_TARGET_NAME}> "$<$<CONFIG:Debug>:${CMAKE_BINARY_DIR}/dist/lib/Debug/$<TARGET_FILE_NAME:${WASABI_TARGET_NAME}>>$<$<CONFIG:Release>:${CMAKE_BINARY_DIR}/dist/lib/Release/$<TARGET_FILE_NAME:${WASABI_TARGET_NAME}>>"
             COMMENT "Building the dist/ folder"
+            DEPENDS "$<TARGET_FILE:${WASABI_TARGET_NAME}>"
         )
         add_custom_target(${TARGET_NAME}
             DEPENDS "${CMAKE_BINARY_DIR}/dist/lib/Debug/${OUTPUT_LIBNAME}" "${CMAKE_BINARY_DIR}/dist/lib/Release/${OUTPUT_LIBNAME}"
@@ -17,8 +18,9 @@ function(build_dist TARGET_NAME WASABI_TARGET_NAME)
         add_custom_command(
             OUTPUT "dist/lib/${OUTPUT_LIBNAME}"
             COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_SOURCE_DIR}/include/Wasabi/" "${CMAKE_BINARY_DIR}/dist/include/Wasabi/"
-            COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:wasabi> "${CMAKE_BINARY_DIR}/dist/lib/$<TARGET_FILE_NAME:${WASABI_TARGET_NAME}>"
+            COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${WASABI_TARGET_NAME}> "${CMAKE_BINARY_DIR}/dist/lib/$<TARGET_FILE_NAME:${WASABI_TARGET_NAME}>"
             COMMENT "Building the dist/ folder"
+            DEPENDS "$<TARGET_FILE:${WASABI_TARGET_NAME}>"
         )
         add_custom_target(${TARGET_NAME}
             DEPENDS "dist/lib/${OUTPUT_LIBNAME}"
