4b9bf231d9d62bb0cf3939106d60116fddf93556
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2019-10-27 15:01:46 +0300

    Implemented basic fireball

--- samples/vertagon/include/vertagon/spells/fireball.hpp
+++ samples/vertagon/include/vertagon/spells/fireball.hpp
@@ -4,10 +4,21 @@
 #include "vertagon/spells/spells.hpp"
 
 class Spell_Fireball : public Spell {
+    WParticles* m_particles;
+    WPointLight* m_light;
+
+    float m_shotTime;
+    WVector3 m_targetOrigin;
+    WVector3 m_targetDirection;
+
 public:
     Spell_Fireball(Vertagon* app);
 
     virtual WError Load() override;
-    virtual bool Update(float fDeltaTime) override;
+    virtual void Update(float fDeltaTime) override;
     virtual void Cleanup() override;
+    virtual bool IsAlive() override;
+
+    void SetIdlePosition(WVector3 position);
+    void SetDirection(WVector3 origin, WVector3 direction);
 };
